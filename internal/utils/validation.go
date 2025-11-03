205523b2be8caf3624172bc4e2bf0c3f2a01b584
Dylan Wilson <dylan@bytepen.com>
2025-11-03 18:02:21 -0500

    Added at sign to username validation

--- internal/utils/validation.go
+++ internal/utils/validation.go
@@ -2,8 +2,8 @@ package utils
 
 import "regexp"
 
-// Username validation regex: only lowercase a-z, digits 0-9, dots, and hyphens
-var usernameRegex = regexp.MustCompile(`^[a-z0-9.-]+$`)
+// Username validation regex: only lowercase a-z, digits 0-9, dots, hyphens, and at signs
+var usernameRegex = regexp.MustCompile(`^[a-z0-9.-@]+$`)
 
 // IsValidUsername checks if username contains only lowercase letters, digits, dots, and hyphens
 func IsValidUsername(username string) bool {

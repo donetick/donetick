85adf480c8b270809be3c67fb968ee391ec20eb0
Tom Frenzel <admin@tom-frenzel.de>
2025-11-08 12:32:10 +0100

    chore: Add validation for OAuth profile picture URL

--- internal/auth/identity_provider.go
+++ internal/auth/identity_provider.go
@@ -107,7 +107,27 @@ func (i *IdentityProvider) GetUserInfo(ctx context.Context, accessToken string) 
 		userInfo.Email = val.(string)
 	}
 	if val, ok := claims["picture"]; ok {
-		userInfo.Picture = val.(string)
+		pictureURL := val.(string)
+		isValid := isPictureURLValid(pictureURL)
+		if isValid {
+			userInfo.Picture = pictureURL
+		}
 	}
 	return &userInfo, nil
+}
+
+// Check if the provided URL is reachable and returns a valid image
+func isPictureURLValid(url string) bool {
+	resp, err := http.Get(url)
+	if err != nil {
+		return false
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return false
+	}
+
+	contentType := resp.Header.Get("Content-Type")
+	return strings.HasPrefix(contentType, "image/")
 }

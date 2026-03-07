2a43af2c3788321ee07ad9092c19a6312082c264
Niklas Beinghaus <code@niklasbeinghaus.com>
2026-03-07 04:56:03 +0100

    feat: allow configuring custom fromEmail and separate users for SMTP (#325)
    * feat: allow configuring custom fromEmail and separate users for SMTP
    
    Signed-off-by: Niklas Beinghaus <hi@niklasbeinghaus.com>
    
    * fix: syntax, extend EmailSender type
    
    Signed-off-by: Niklas Beinghaus <hi@niklasbeinghaus.com>
    
    ---------
    
    Signed-off-by: Niklas Beinghaus <hi@niklasbeinghaus.com>

--- config/config.go
+++ config/config.go
@@ -166,11 +166,12 @@ type FCMConfig struct {
 	ProjectID       string `json:"project_id" mapstructure:"project_id"`
 }
 type EmailConfig struct {
-	Email   string `mapstructure:"email"`
-	Key     string `mapstructure:"key"`
-	Host    string `mapstructure:"host"`
-	Port    int    `mapstructure:"port"`
-	AppHost string `mapstructure:"appHost"`
+	Email   	string `mapstructure:"email"`
+	User		string `mapstructure:"user"`
+	Key     	string `mapstructure:"key"`
+	Host    	string `mapstructure:"host"`
+	Port    	int    `mapstructure:"port"`
+	AppHost 	string `mapstructure:"appHost"`
 }
 
 type OAuth2Config struct {

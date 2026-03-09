donetick/donetick

commit 2b5c6bb6c6a6768642d51cf0c5053020f33ace7f

Feature/simple health check api (#527)
* Add simple health-check endpoint Fixes #516

* Put the health route on /api/v1/health

* Add test endpoint documentation to README.MD

main.go
@@ -343,5 +343,10 @@ func newServer(lc fx.Lifecycle, cfg *config.Config, db *gorm.DB, notifier *notif
 		},
 	})
 
+	//Simple health-check endpoint
+	r.GET("/api/v1/health", func(c *gin.Context) {
+		c.Status(http.StatusOK) // 200 - OK if server started
+	})
+
 	return r
 }

commit e7f280e9fab17078eeb07902b39649066f603e26

Fix lint errors introduced in #527

config/config.go
@@ -166,12 +166,12 @@ type FCMConfig struct {
 	ProjectID       string `json:"project_id" mapstructure:"project_id"`
 }
 type EmailConfig struct {
-	Email   	string `mapstructure:"email"`
-	User		string `mapstructure:"user"`
-	Key     	string `mapstructure:"key"`
-	Host    	string `mapstructure:"host"`
-	Port    	int    `mapstructure:"port"`
-	AppHost 	string `mapstructure:"appHost"`
+	Email   string `mapstructure:"email"`
+	User    string `mapstructure:"user"`
+	Key     string `mapstructure:"key"`
+	Host    string `mapstructure:"host"`
+	Port    int    `mapstructure:"port"`
+	AppHost string `mapstructure:"appHost"`
 }
 
 type OAuth2Config struct {

internal/email/sender.go
@@ -11,14 +11,14 @@ import (
 )
 
 type EmailSender struct {
-	client  *gomail.Dialer
-	appHost string
+	client   *gomail.Dialer
+	appHost  string
 	fromMail string
 }
 
 func NewEmailSender(conf *config.Config) *EmailSender {
 
-    // if no user is set, use the email as user
+	// if no user is set, use the email as user
 	if conf.EmailConfig.User == "" {
 		conf.EmailConfig.User = conf.EmailConfig.Email
 	}
@@ -29,9 +29,9 @@ func NewEmailSender(conf *config.Config) *EmailSender {
 
 	// auth := smtp.PlainAuth("", conf.EmailConfig.Email, conf.EmailConfig.Password, host)
 	return &EmailSender{
-        fromMail: conf.EmailConfig.Email,
-		client:  client,
-		appHost: conf.EmailConfig.AppHost,
+		fromMail: conf.EmailConfig.Email,
+		client:   client,
+		appHost:  conf.EmailConfig.AppHost,
 	}
 }
 

main.go
@@ -343,7 +343,7 @@ func newServer(lc fx.Lifecycle, cfg *config.Config, db *gorm.DB, notifier *notif
 		},
 	})
 
-	//Simple health-check endpoint
+	// Simple health-check endpoint
 	r.GET("/api/v1/health", func(c *gin.Context) {
 		c.Status(http.StatusOK) // 200 - OK if server started
 	})